abc0c78432e0b336315cec85a343ce3421a58896
heyscottn <heyscottn@gmail.com>
2024-04-28 11:52:55 -0500

    mm

--- Hanoi.java
+++ Hanoi.java
@@ -8,9 +8,12 @@ public class Hanoi{
 		Scanner input = new Scanner(System.in);
 		System.out.print("\nHow many disks are in the tower  ===>>  ");
 		int disks = input.nextInt();
-		solveHanoi('A','B','C',disks);
+
+		solveHanoi('A', 'B', 'C', disks);
+
 		System.out.println("\n\nEXECUTION TERMINATED " + numTimes);
 	} // close main
+
 
 
 	public static void solveHanoi(char s, char t, char d, int n){
@@ -18,12 +21,8 @@ public class Hanoi{
 		// t - temporary peg
 		// d - destination peg
 		// n - number of disks
-		numTimes++;
-		if (n != 0){
-			solveHanoi(s, d, t, n-1); // move n-1 disks from A to B
-			System.out.println("Move Disk " + n + " From Peg " + s + " to Peg " + d);
-			solveHanoi(t, s, d, n-1); // move n-1 disks from B to C
-		}
+
+
 	}
 
 }
